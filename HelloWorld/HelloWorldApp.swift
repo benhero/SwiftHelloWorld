c4b383e4228336d54c0a89e55b06d21bc5ebad79
chenbenbin <chenbenbin@bytedance.com>
2022-05-29 23:45:49 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Ben on 2022/5/29.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
